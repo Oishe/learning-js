18bb45ed00623518c94302985f7b9a3d881fc690
Oishe <farhanoishe@gmail.com>
2017-10-17 01:09:14 -0400

    Completed a two way binging using NgModel Imported from FormsModule

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; // NgModel here
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [
+    BrowserModule,
+    FormsModule // import FormsModule before binding with [(ngModel)]
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
